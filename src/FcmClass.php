c84bf6fcb29ac4ed1ec032caa59bdde99fff55c4
Kenneth John Rosales <kenjos75@yahoo.com>
2018-10-31 16:55:49 +0800

    Update functionality.

--- src/FcmClass.php
+++ src/FcmClass.php
@@ -9,19 +9,69 @@
 */
 class FcmClass{
 
-   /**  @var string $server_key  */
-   private $server_key = 'AAAA4Br5JWo:APA91bFlRTFtEMzJxxLH4cteoz4zl4KW45PLU_G4mL21Z72woIyOzAp63AOSqOBcZb1gJxRbQFPEAFK0GTfaavQTIDpmKavVLKjBxE-qPItSlQYbCeMyraFURTAIqjWflGjEAuEsFNRY';
-  
+  /**  @var string $server_key  */
+  private $server_key = "";
+
+
+  /**  @var string $fcm_url  */
+  private $url = "https://fcm.googleapis.com/fcm/send";
+
+
   /**
   * Send function 
   *
   * Send a message to the recepient device,
   *
-  * @param string $param1 A string containing the parameter, do this for each parameter to the function, make sure to make it descriptive
-  *
-  * @return string
+  * @param string $to The receiver client app id
+  * @param array $data The notification data
+  * @return boolean
   */
-   public function send(){
-			return "Hello World";
-   }
+  public function send($to='',$data=array()){
+
+      $defaultData = array("title"=>"Default Title","body"=>"Default Data");
+      $check_data = array("title","body");
+
+      if($data!=array() && $to!='' && $this->server_key!=''){
+
+        foreach ($check_data as $value) {
+          if(isset($data[$value]))
+            $defaultData[$value] = $data[$value];
+        }
+
+        $headers = array("Content-Type: application/json","Authorization: key=".$this->server_key);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($s,CURLOPT_POST,true); 
+        curl_setopt($s,CURLOPT_POSTFIELDS,json_encode(array("notification"=>$defaultData,"to"=>$to)); 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($ch);
+        curl_close($curl);
+        
+      }
+      else
+        return false;
+
+      return true;
+  }
+
+  /**
+  * Constructor
+  *
+  * Instantiate the server key,
+  *
+  * @param string $to The receiver client app id
+  * @param array $data The notification data
+  * @return FormClass
+  */
+  public function __construct($server_key=''){
+    if($server_key!='')
+      $this->server_key = $server_key;
+    
+    return $this;
+
+  }
+
+
+
 }
